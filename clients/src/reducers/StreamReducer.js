9eb722a4ad47da2bab38c0cab33c4332dc7a6553
TJ <abongsjoel@gmail.com>
2021-01-06 12:02:13 +0100

    Handle fetching, creating and updating

---
+++ clients/src/reducers/StreamReducer.js
@@ -0,0 +1,21 @@
+import {
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  CREATE_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from '../actions/types';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = {}, action) => {
+  switch(action.type) {
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    default:
+      return state;
+  }
+}
