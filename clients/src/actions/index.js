273fea0b06cc2a2a6805c73a7c5c1d52f4b55f76
TJ <abongsjoel@gmail.com>
2021-01-06 11:26:17 +0100

    Bulk action creators

--- clients/src/actions/index.js
+++ clients/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
+import { 
+  SIGN_IN, 
+  SIGN_OUT, 
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM 
+} from './types';
 
 export const signIn = (userId) => {
   return {
@@ -19,3 +27,27 @@ export const createStream = formValues => async dispatch => {
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get('/streams');
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+}
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+}
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+}
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+}
