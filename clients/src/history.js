f8bb9c45be2d21e03ceb12400949ff34593c5cac
TJ <abongsjoel@gmail.com>
2021-01-07 11:44:41 +0100

    Create a Browser History Object

---
+++ clients/src/history.js
@@ -0,0 +1,7 @@
+// import createHistory from 'history/createBrowserHistory';
+
+// export default createHistory();
+
+import { createBrowserHistory } from 'history';
+
+export default createBrowserHistory();
