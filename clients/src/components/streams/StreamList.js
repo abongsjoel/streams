4a92212c2e6fc7ba54d6ea41a756a04e0bdc6296
TJ <abongsjoel@gmail.com>
2021-01-07 16:37:23 +0100

    Using Portals

--- clients/src/components/streams/StreamList.js
+++ clients/src/components/streams/StreamList.js
@@ -16,7 +16,7 @@ class StreamList extends React.Component {
           <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
             Edit
           </Link>
-          <Link className="ui button negative">
+          <Link to="/" className="ui button negative">
             Delete
           </Link>
         </div>
