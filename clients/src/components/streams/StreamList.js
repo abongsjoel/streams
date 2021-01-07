9eb129871ccc0515bd3ccdd24f68482889d32bc8
TJ <abongsjoel@gmail.com>
2021-01-07 10:15:54 +0100

    Fetch a list of all streams

--- clients/src/components/streams/StreamList.js
+++ clients/src/components/streams/StreamList.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-  return (
-    <div>
-      StreamList
-    </div>
-  )
-}
+class StreamList extends React.Component {
 
-export default StreamList;
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  render() {
+    return (
+      <div>
+        StreamList
+      </div>
+    )
+  }
+};
+
+export default connect(null, { fetchStreams })(StreamList);
