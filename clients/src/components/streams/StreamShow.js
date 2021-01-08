b90cd738051ee8646d321f0b90dacbb5af5edfae
TJ <abongsjoel@gmail.com>
2021-01-08 14:42:41 +0100

    Create an FLV player2

--- clients/src/components/streams/StreamShow.js
+++ clients/src/components/streams/StreamShow.js
@@ -14,10 +14,12 @@ class StreamShow extends React.Component {
     const { id } = this.props.match.params;
 
     this.props.fetchStream(id);
-    flv.createPlayer({
+    this.player = flv.createPlayer({
       type: 'flv',
       url: `http://localhost:8000/live/${id}.flv`
     });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
